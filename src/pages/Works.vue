f5794f6e22147a8bc3d04f32bb7e597a83fe4e99
Karlos <aixegorri@gmail.com>
2019-04-07 23:23:20 +0200

    Add grid works

--- src/pages/Works.vue
+++ src/pages/Works.vue
@@ -1,12 +1,17 @@
 <template>
 	<Layout>
-		<ul>
-			<li v-for="post in $page.posts.edges" :key="post.id">
+		<section class="works">
+			<article class="works__item" v-for="post in $page.posts.edges" :key="post.id">
 				<g-link :to="post.node.path">
-					{{ post.node.title }}
+					<figure>
+						<g-image src="~/assets/img/works/aita/cover.jpg" :alt="post.node.title" />
+					</figure>
+					<div class="works__hover-state">
+						<h3 class="works__title">{{ post.node.title }}</h3>
+					</div>
 				</g-link>
-			</li>
-		</ul>
+			</article>
+		</section>
 	</Layout>
 </template>
 
@@ -31,3 +36,74 @@ export default {
 	}
 }
 </script>
+
+<style lang="scss">
+	.works {
+		display: grid;
+		grid-template-columns: repeat(auto-fit, minmax(15rem, 1fr));
+		grid-column-gap: $gutter-width;
+		grid-row-gap: $gutter-width;
+	}
+
+	.works__item {
+		width: 100%;
+		height: em(200);
+
+		&:hover {
+			.works__hover-state {
+				opacity: .9;
+			}
+			.works__title {
+				opacity: 1;
+				transform: translateY(0);
+			}
+		}
+
+		> a {
+			display: block;
+			position: relative;
+			width: 100%;
+			height: 100%;
+		}
+
+		figure {
+			position: absolute;
+			top: 0;
+			left: 0;
+			width: 100%;
+			margin: 0;
+
+			img {
+				width: 100%;
+				height: em(200);
+				margin: 0;
+				object-fit: cover;
+			}
+		}
+	}
+
+	.works__hover-state {
+		display: flex;
+		align-items: flex-end;
+		position: absolute;
+		left: 1em;display: flex;
+		right: 1em;
+		top: 1em;
+		bottom: 1em;
+		width: auto;
+		height: auto;
+		background-color: $color-primary;
+		padding: $padding;
+		opacity: 0;
+		transition: $animation-hover;
+	}
+
+	.works__title {
+		margin: 0;
+		opacity: 0;
+		transform: translateY(1em);
+		transition: $animation-hover;
+		transition-delay: $animation-hover / 2;
+	}
+</style>
+
--- work/HutsuneakVacios.md
+++ work/HutsuneakVacios.md
@@ -2,7 +2,7 @@
 title: Hutsuneak / Vacíos
 ---
 
-# Aita
+# Hutsuneak / Vacíos
 
 Hutsuneak/Vacíos parte de fotogramas que han sufrido una pérdida de información en el proceso de la captación de la imagen en movimiento, y a su vez, también, la pérdida de información en el proceso de transformación de una imagen digital a una manual. Estas imágenes que han sido despojadas de color e intensidad son convertidas en cajas de luces, que contienen un espacio de vacío real, físico, el cual es iluminado. La imagen va acompañada de frases en inglés extraídas de la canción God de John Lennon, que ayuda al espectador a ver la obra con el ritmo de reflexión al que evoca.
 
