c323f84a06a03986467520500a076174260883ce
Karlos <aixegorri@gmail.com>
2019-04-07 18:30:22 +0200

    Create works section

--- src/pages/Works.vue
+++ src/pages/Works.vue
@@ -1,3 +1,17 @@
 <template>
-
+	<Layout>
+		<ul>
+			<li>
+				<g-link to="/works/aita">Aita</g-link>
+			</li>
+		</ul>
+	</Layout>
 </template>
+
+<script>
+export default {
+	metaInfo: {
+		title: 'Works'
+	}
+}
+</script>
