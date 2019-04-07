6fce256892cc0086f5ce6f1dbfbe8c90316464c9
Karlos <aixegorri@gmail.com>
2019-04-07 17:18:58 +0200

    Add node-sass

--- src/main.js
+++ src/main.js
@@ -3,6 +3,9 @@
 
 import DefaultLayout from '~/layouts/Default.vue'
 
+// Import Styles
+import '~/assets/scss/styles.scss'
+
 export default function (Vue, { router, head, isClient }) {
   // Set default layout as a global component
   Vue.component('Layout', DefaultLayout)
