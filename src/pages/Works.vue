9fb476eb716cfe57c322d1b3e2cdd611c5ec4f88
Karlos <aixegorri@gmail.com>
2019-04-08 15:12:21 +0200

    Add main title

--- src/pages/Works.vue
+++ src/pages/Works.vue
@@ -1,17 +1,21 @@
 <template>
-	<Layout>
-		<section class="works">
-			<article class="works__item" v-for="post in $page.posts.edges" :key="post.id">
-				<g-link :to="post.node.path">
-					<figure>
-						<g-image :src="post.node.thumbnail" :alt="post.node.title" />
-					</figure>
-					<div class="works__hover-state">
-						<h3 class="works__title">{{ post.node.title }}</h3>
-					</div>
-				</g-link>
-			</article>
-		</section>
+	<Layout class="page--works">
+		<div class="container">
+			<h1 class="main__title">Works</h1>
+			<!-- Works Grid -->
+			<section class="works">
+				<article class="works__item" v-for="post in $page.posts.edges" :key="post.id">
+					<g-link :to="post.node.path">
+						<figure>
+							<g-image :src="post.node.thumbnail" :alt="post.node.title" />
+						</figure>
+						<div class="works__hover-state">
+							<h3 class="works__title">{{ post.node.title }}</h3>
+						</div>
+					</g-link>
+				</article>
+			</section>
+		</div>
 	</Layout>
 </template>
 
