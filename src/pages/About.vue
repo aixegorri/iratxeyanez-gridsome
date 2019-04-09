eea4a11635a87bc24f729d7ab0200cc1eb9431fb
Karlos <aixegorri@gmail.com>
2019-04-09 11:49:16 +0200

    Add bio content

--- src/pages/About.vue
+++ src/pages/About.vue
@@ -1,8 +1,20 @@
 <template>
-  <Layout>
-    <h1>About us</h1>
-    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Error doloremque omnis animi, eligendi magni a voluptatum, vitae, consequuntur rerum illum odit fugit assumenda rem dolores inventore iste reprehenderit maxime! Iusto.</p>
-  </Layout>
+  	<Layout class="page--about">
+		<div class="container">
+			<h3 class="main__title">About</h3>
+			<section class="bio">
+				<figure class="bio__image">
+					<g-image src="~/assets/img/bio@2x.jpg" alt="Iratxe Yañez Picture" />
+				</figure>
+				<article class="bio__article">
+					<h4 class="bio__title">Iratxe Yañez</h4>
+					<p>Mi trabajo se centra en el campo experimental de la pintura y tiene como origen el propio proceso del sujeto creador.</p>
+					<p>Mi práctica experimental como artista plástica parte de imágenes fotográficas como excusa de creación. Encontramos imágenes fílmicas, como imágenes fotográficas personales e imágenes pertenecientes al imaginario propio, que son constituyentes de una memoria particular.</p>
+					<p>Utilizo la imagen fotográfica como punto de partida, para después despojarla de su narrativa y así poder centrarnos en su plástica. Mediante el proceso creativo se pierden los significantes originales de la imagen, se pierde toda referencia visible o aparente con la imagen original, con la memoria de la imagen, y así , éste objeto plástico va cobrando presencia propia.</p>
+				</article>
+			</section>
+		</div>
+  	</Layout>
 </template>
 
 <script>
