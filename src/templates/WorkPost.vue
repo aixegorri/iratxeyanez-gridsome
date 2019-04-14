94ece3c4187108f5924b2708a8252444e9a65e73
Karlos <aixegorri@gmail.com>
2019-04-14 19:53:58 +0200

    Fix vue-carousel component without SSR support

--- src/templates/WorkPost.vue
+++ src/templates/WorkPost.vue
@@ -4,21 +4,23 @@
 			<h2 class="main__title">{{ $page.post.title }}</h2>
 			<article class="work-description" v-html="$page.post.content"></article>
 			<!-- Image carousel -->
-			<carousel
-				class="work-carousel"
-				:autoplay="true"
-				:autoplayTimeout="8000"
-				:autoplayHoverPause="true"
-				:easing="'ease-in'"
-				:loop="true"
-				:perPage="1">
-				<slide v-for="image in $page.post.workImages" :key="image.id">
-					<figure>
-						<g-image class="img-responsive work-carousel__image" :src="image.src" :alt="image.src" />
-						<figcaption class="work-carousel__caption">{{ image.caption }}</figcaption>
-					</figure>
-				</slide>
-			</carousel>
+			<ClientOnly>
+				<carousel
+					class="work-carousel"
+					:autoplay="true"
+					:autoplayTimeout="8000"
+					:autoplayHoverPause="true"
+					:easing="'ease-in'"
+					:loop="true"
+					:perPage="1">
+					<slide v-for="image in $page.post.workImages" :key="image.id">
+						<figure>
+							<g-image class="img-responsive work-carousel__image" :src="image.src" :alt="image.src" />
+							<figcaption class="work-carousel__caption">{{ image.caption }}</figcaption>
+						</figure>
+					</slide>
+				</carousel>
+			</ClientOnly>
 		</div>
 	</Layout>
 </template>
@@ -47,9 +49,15 @@ export default {
 	},
 
 	components: {
-   	Carousel,
-   	Slide
-  }
+	Carousel: () =>
+		import ('vue-carousel')
+		.then(m => m.Carousel)
+		.catch(),
+	Slide: () =>
+		import ('vue-carousel')
+		.then(m => m.Slide)
+		.catch()
+	}
 }
 </script>
 
