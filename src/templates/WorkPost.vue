4b3608c2d2a28d304e3d5639068d7dc889afdbc7
Karlos <aixegorri@gmail.com>
2019-04-14 18:33:03 +0200

    Add vue carousel

--- src/templates/WorkPost.vue
+++ src/templates/WorkPost.vue
@@ -3,6 +3,20 @@
 		<div class="container">
 			<h2 class="main__title">{{ $page.post.title }}</h2>
 			<article class="work-description" v-html="$page.post.content"></article>
+			<!-- Image carousel -->
+			<carousel
+				:perPage="1">
+				<slide>
+					<figure>
+						<g-image src="/uploads/works/escenas/escenas_01.jpg" alt="" />
+					</figure>
+				</slide>
+				<slide>
+					<figure>
+						<g-image src="/uploads/works/escenas/escenas_02.jpg" alt="" />
+					</figure>
+				</slide>
+			</carousel>
 		</div>
 	</Layout>
 </template>
@@ -17,12 +31,19 @@ query WorkPost ($path: String!) {
 </page-query>
 
 <script>
+import { Carousel, Slide } from 'vue-carousel'
+
 export default {
 	metaInfo() {
 		return {
 			title: this.$page.post.title
 		}
-	}
+	},
+
+	components: {
+   	Carousel,
+   	Slide
+  }
 }
 </script>
 
